8b715bf36003fbf83ab9446bbdadd3ed3980a56c
SolarCoder <74742542+solar-coder@users.noreply.github.com>
2020-11-21 17:51:21 -0500

    Add files via upload

---
+++ f-string.py
@@ -0,0 +1,13 @@
+# Author: SolarCoder
+# Instagram | @solarcoder
+
+num_one = int(input("Enter first number: "))
+num_two = int(input("Enter second number: "))
+
+# prefixing the string with an f to indicate an f string.
+# using {} to specify the variables and expression
+print(f"The sum of {num_one} and {num_two} is {num_one + num_two}")
+
+# Output: Enter first number: 3
+#         Enter second number: 3
+#         The sum of 3 and 3 is 6
